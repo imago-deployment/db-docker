05d07b2e462b9057d0065ea3c68e0addb08c0fe0
realmad <realmad@imagotechs.com>
2026-09-15 14:11:43 +0700

    update mysql conf

--- config/mysql/docker-entrypoint-initdb.d/init.sql
+++ config/mysql/docker-entrypoint-initdb.d/init.sql
@@ -5,9 +5,22 @@ CREATE DATABASE IF NOT EXISTS db_imago;
 USE db_imago;
 
 CREATE TABLE t_person (
-    person_id int,
+    person_id VARCHAR(36) PRIMARY KEY,
     last_name varchar(255),
     first_name varchar(255),
     email varchar(255),
     city varchar(255)
 );
+
+INSERT INTO db_imago.t_person (
+    person_id,
+    last_name,
+    first_name,
+    email,
+    city )
+VALUES (
+    UUID(),
+    'Zhao',
+    'Andrew',
+    'andrew@imago.us',
+    'KL');
