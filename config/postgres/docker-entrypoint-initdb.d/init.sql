959234ea36fecb550507b82e8ad6ada55621ac91
realmad <realmad@imagotechs.com>
2026-09-15 15:02:09 +0700

    update Postgresql conf

---
+++ config/postgres/docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,13 @@
+CREATE SCHEMA IF NOT EXISTS imago;
+
+-- Create a table
+CREATE TABLE IF NOT EXISTS imago.t_person (
+    person_id uuid NOT NULL DEFAULT uuidv7(),
+    person_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+-- Insert predefined data
+INSERT INTO imago.t_person (person_name, email) VALUES
+    ('andrew', 'andrew@example.com'),
+    ('bob', 'bob@example.com');
